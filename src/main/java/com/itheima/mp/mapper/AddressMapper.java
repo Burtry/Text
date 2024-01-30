0b869aca1a9c4cd230d3e99f580dbb76c273f412
Burtry <1971797331@qq.com>
2024-01-30 15:50:34 +0800

    mp-自动生成代码2

---
+++ src/main/java/com/itheima/mp/mapper/AddressMapper.java
@@ -0,0 +1,16 @@
+package com.itheima.mp.mapper;
+
+import com.itheima.mp.domain.po.Address;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Burtry
+ * @since 2024-01-30
+ */
+public interface AddressMapper extends BaseMapper<Address> {
+
+}
