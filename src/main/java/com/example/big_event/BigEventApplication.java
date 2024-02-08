eb189a8abc17a4fd750e04dbbb5efc037f200f5f
Burtry <1971797331@qq.com>
2024-02-08 21:16:10 +0800

    update_code---摸鱼lo

--- src/main/java/com/example/big_event/BigEventApplication.java
+++ src/main/java/com/example/big_event/BigEventApplication.java
@@ -1,6 +1,5 @@
 package com.example.big_event;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
