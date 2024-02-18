bd9c5442bac2c21ff8d81ad452af8d2f8be1623b
Burtry <1971797331@qq.com>
2024-02-18 21:12:31 +0800

    vue基础使用

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <TextVue></TextVue>
+</template>
+
+<script setup>
+import TextVue from './components/TextVue.vue'
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/utils/request.js
@@ -0,0 +1,18 @@
+//定制请求的实例
+import axios from 'axios';
+const baseURL = 'http://localhost:8082';
+const instance = axios.create({ baseURL });
+
+
+//添加响应拦截器
+instance.interceptors.response.use(
+    res => {
+        return res.data;
+    },
+    err => {
+        console.log("服务异常");
+        return Promise.reject(err); //将异步状态转换成失败的状态。
+    }
+)
+
+export default instance;
