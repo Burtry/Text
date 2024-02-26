83459098c6f8565162b53c514ac59678c9129432
Burtry <1971797331@qq.com>
2024-02-26 19:17:05 +0800

    添加router依赖

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <LoginVue></LoginVue>
+    <LayoutVue></LayoutVue>
   </div>
 </template>
 <script setup>
 import LoginVue from "./views/Login.vue";
+import LayoutVue from './views/Layout.vue'
 </script>
 
 <style></style>
